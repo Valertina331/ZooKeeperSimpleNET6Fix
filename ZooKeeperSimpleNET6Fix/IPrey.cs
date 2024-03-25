94f8c21cf2c45392e38fbc8c5f6e55d1b9efd3c9
ColdEgg-T T <valertina331@gmail.com>
2024-03-25 02:16:43 -0400

    final version

--- ZooKeeperSimpleNET6Fix/IPrey.cs
+++ ZooKeeperSimpleNET6Fix/IPrey.cs
@@ -43,6 +43,49 @@ namespace ZooKeeperSimpleNET6Fix
             }
         }
 
+        public void TotalFlee(Animal prey, int x, int y, string predator)
+        {
+            // make the first move
+            Random random = new Random();
+            Direction move = Flee1(x, y, predator);
+            if (Game.Seek(x, y, move, predator, 1) == 0)
+            {
+                Game.Move(prey, move, 1);
+            }
+
+            // make sure the prey will not flee back to the original suqare
+            List<Direction> possibleDirections = new List<Direction> { Direction.up, Direction.down, Direction.left, Direction.right };
+            if (move == Direction.up)
+            {
+                possibleDirections.Remove(Direction.down);
+            }
+            else if (move == Direction.down)
+            {
+                possibleDirections.Remove(Direction.up);
+            }
+            else if (move == Direction.left)
+            {
+                possibleDirections.Remove(Direction.right);
+            }
+            else if (move == Direction.right)
+            {
+                possibleDirections.Remove(Direction.left);
+            }
+
+            // if the mouse can countinue moving, the direction will not change,
+            // if cannot, it will choose a possible direction randomly.
+            if (Game.Seek(x, y, move, predator, 2) == 0)
+            {
+                Game.Move(prey, move, 1);
+            }
+            else if (possibleDirections.Count > 0)
+            {
+                Direction moveDirection = possibleDirections[random.Next(possibleDirections.Count)];
+                Game.Move(prey, moveDirection, 1);
+            }
+        }
+
+        // will not use in mouse
         public bool Flee(Animal prey, int x, int y, string predator)
         {
             if (Game.Seek(x, y, Direction.up, predator, 1) > 0)
